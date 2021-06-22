c0dc7e41d45e066e6086b79fce22d0145c78c74c
Fady Ramzy <fady.h.ramzy@gmail.com>
2021-06-22 18:41:45 +0200

    Update RecipesViewModel.swift

--- RecipiesRX/Recipes/ViewModel/RecipesViewModel.swift
+++ RecipiesRX/Recipes/ViewModel/RecipesViewModel.swift
@@ -9,8 +9,15 @@ import Foundation
 import RxSwift
 
 class RecipesViewModel{
+    
     let title = "Recipes"
     private let recipes = Recipe.recipes
+    
+     /* 
+        1. This observable can be converted to [Publish Subject] because if you want to emit any new events inside it you will be able to do that with the subject you can do that 
+        2. Second option here you can create your own observable
+    */
+    
     let recipeObservable: Observable<[RecipeUIModel]>
     
     func selectedRecipe(for index: IndexPath, route: RecipeRoute){
