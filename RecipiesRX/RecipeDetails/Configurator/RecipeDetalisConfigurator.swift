f524846650a1e2269feeb72ce02ac501e19ae276
Fady Ramzy <fady.h.ramzy@gmail.com>
2021-06-22 19:17:08 +0200

    Update RecipeDetalisConfigurator.swift

--- RecipiesRX/RecipeDetails/Configurator/RecipeDetalisConfigurator.swift
+++ RecipiesRX/RecipeDetails/Configurator/RecipeDetalisConfigurator.swift
@@ -7,15 +7,37 @@
 
 import Foundation
 
-class RecipeDetailsConfigurator{
+// This could be struct not a class 
+
+class RecipeDetailsConfigurator {
+    
     let recipe: Recipe
     
     init(recipe: Recipe) {
         self.recipe = recipe
     }
     
-    func config(view: RecipeDetailsViewController){
+    func config(view: RecipeDetailsViewController) is {
         let viewModel = RecipeDetailsViewModel(recipe: recipe)
         view.recipeViewModel = viewModel
     }
 }
+
+/*
+enum RecipeConfigurator: Configurator {
+    
+    case list
+    case details(recipe: Recipe)
+    
+    var destinationViewController: UIViewController {
+        switch self {
+        case .details(let recipe):
+            // Here return the recipe details
+        case .list:
+        // Here return the list viewcontroller
+        }
+    }
+}
+
+*/
+
